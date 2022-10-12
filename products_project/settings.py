e2f48cbdfdda560d942ffa2ccf69301a8b9b5af1
Nathaniel-Mueller <nathaniel.15853.clsec.8@gmail.com>
2022-10-11 21:48:03 -0700

    fixed local_settings import

--- products_project/settings.py
+++ products_project/settings.py
@@ -1,3 +1,5 @@
+
+
 """
 Django settings for products_project project.
 
@@ -18,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s+p0r=eclfyid4g3h343bt^5w1_gy()_1g^ee^f#(i7s!8&8pb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -70,16 +69,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'products_project.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -121,3 +110,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+try:
+    from products_project.local_settings import *
+except ImportError:
+    pass
