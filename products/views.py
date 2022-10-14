14c114193d904ec231be342568167f968ac35800
Nathaniel-Mueller <nathaniel.15853.clsec.8@gmail.com>
2022-10-13 20:36:10 -0700

    Added 'POST' method to views.py

--- products/views.py
+++ products/views.py
@@ -1,12 +1,20 @@
+from math import prod
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import *
 from .serializers import *
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def productsList(request):
     
-    products = Product.objects.all()
-    serializer = ProductSerializer(products,many=True)
     
-    return Response(serializer.data)
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products,many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
